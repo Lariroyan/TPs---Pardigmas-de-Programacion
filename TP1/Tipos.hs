4807d9ffe63bceb2a503c3d0b184647b19e9897e
Lari <94306835+Lariroyan@users.noreply.github.com>
2024-08-19 11:27:57 -0300

    Update Tipos.hs

--- TP1/Tipos.hs
+++ TP1/Tipos.hs
@@ -1,1 +1,10 @@
+module Tipos
+    where
+        import Data.Maybe
+        import Data.List
+        import Data.Char
 
+
+        type Duracion = Int
+        type Departamento = String
+        type Nombre = String
