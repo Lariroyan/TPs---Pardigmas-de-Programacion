7c27a2e2c6fcad8762007dc846a8ec1599a096d6
Lari <94306835+Lariroyan@users.noreply.github.com>
2024-08-20 09:21:26 -0300

    Update FileSystem.hs

--- TP1/FileSystem.hs
+++ TP1/FileSystem.hs
@@ -6,13 +6,18 @@ module FileSystem ( FileSystem, nuevoF, departamentosF, anunciosF, agregarAnunci
 
     data FileSystem = FS [Departamento] [Anuncio] deriving (Eq, Show)
 
+   
     nuevoF :: FileSystem                                              -- permite obtener un nuevo FileSystem
     nuevoF  =  FS [] []
     departamentosF :: FileSystem -> [ Departamento ]                  -- dado un FileSystem retorna los departamentos que incluye
     departamentosF (FS departamentos _) = departamentos
 
     anunciosF :: FileSystem -> [ Anuncio ]                            -- dado un FileSystem retorna los anuncios que incluye
+    anunciosF (FS _ anuncios) = anuncios 
+    
     agregarAnuncioF :: Anuncio -> FileSystem -> FileSystem            -- permite agregar un anuncio  
+    agregarAnuncioF (FS departamentos anuncios) = FS departamentos (anuncio:anuncios)
+    
     sacarAnuncioF :: Anuncio -> FileSystem -> FileSystem              -- permite eliminar un anuncio
     agregarDepartamentoF :: Departamento -> FileSystem -> FileSystem  -- permite agregar un departamento
     sacarDepartamentoF :: Departamento -> FileSystem -> FileSystem    -- permite eliminar un departamento
