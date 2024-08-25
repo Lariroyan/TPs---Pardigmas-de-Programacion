9550166ead24bc42df6a138cb63b3f5dd2283c2d
Lari <94306835+Lariroyan@users.noreply.github.com>
2024-08-25 14:48:37 -0300

    Update Prompter.hs

--- TP1/Prompter.hs
+++ TP1/Prompter.hs
@@ -3,15 +3,36 @@ module Prompter ( Prompter, nuevoP, archivosR, departamentosP, configurarP, anun
 
     import Tipos
     import Anuncio
-    import FileSystem
+    import FilesSystem
 
-    data Prompter = Pro FileSystem  [Departamento] Int deriving (Eq, Show)
+    data Prompter = Pro FilesSystem  [Departamento] Int deriving (Eq, Show)
 
-    nuevoP :: FileSystem -> Prompter                       -- permite obtener un nuevo Prompter en base a un FileSystem
-    archivosR :: Prompter -> FileSystem                    -- dado un prompter retorna su fileSystem
+    nuevoP :: FilesSystem -> Prompter                       -- permite obtener un nuevo Prompter en base a un FileSystem
+    nuevoP fs = Pro fs [] 0
+    
+    archivosR :: Prompter -> FilesSystem                    -- dado un prompter retorna su fileSystem
+    archivosR (Pro fs _ _) = fs
+
     departamentosP :: Prompter -> [Departamento]           -- dado un prompter retorna sus departamentos
+    departamentosP (Pro _ departamentos _) = departamentos
+
     configurarP :: Prompter -> [Departamento] -> Prompter  -- Prepara el prompter para emitir los anuncios en los departementos indicados
+    configurarP (Pro fs _ idx) nuevosDeps = Pro fs nuevosDeps idx
+
     anunciosP :: Prompter ->  [Nombre]                      -- entrega la lista de nombres de anuncios configurados
+    anunciosP (Pro fs deptos _) = let anuncios = anunciosF fs in [nombreA a | a <- anuncios, aplicaA deptos a]
+
     showP :: Prompter -> Anuncio                           -- muestra el anuncio actual 
+    showP (Pro fs deptos index) 
+        | null validAnuncios = error "No hay anuncios válidos para los departamentos configurados"
+        | index < 0 || index >= length validAnuncios = error "Índice fuera de rango"
+        | otherwise = validAnuncios !! index
+         where
+             anuncios = anunciosF fs
+             validAnuncios = filter (`aplicaA` deptos) anuncios
+    
     avanzarP :: Prompter -> Prompter                       -- pasa al siguiente anuncio
     duracionP :: Prompter -> Duracion                      -- indica la duracion total de los anuncios configurados  
+    duracionP (Pro fs deptos _) = sum (map duracionA validAnuncios)
+     where
+      validAnuncios = filter (`aplicaA` deptos) (anunciosF fs)
