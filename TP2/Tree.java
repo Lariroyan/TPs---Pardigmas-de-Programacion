0c448ff616573fa5598945dcdb83f93f00901f99
cami <135481707+cammarze@users.noreply.github.com>
2024-09-09 10:03:21 -0300

    Create Tree.java

---
+++ TP2/Tree.java
@@ -0,0 +1,36 @@
+package tree;
+
+import java.util.List;
+
+public class Tree {
+    public Tree( Object a ) {
+    }
+
+    public List dfs() {
+        return null;
+    }
+
+    public List bfs() {
+        return null;
+    }
+
+    public Tree atLeft( Tree b ) {
+        return null;
+    }
+
+    public Tree atRight( Tree b ) {
+        return null;
+    }
+
+    public Tree right() {
+        return null;
+    }
+
+    public Tree left() {
+        return null;
+    }
+
+    public Object carga() {
+        return null;
+    }
+}
