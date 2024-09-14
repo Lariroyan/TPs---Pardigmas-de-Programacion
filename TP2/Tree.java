fae424ef47cb230739302f7c1c342d971e87cb98
cami <135481707+cammarze@users.noreply.github.com>
2024-09-14 02:39:09 -0300

    Update tree!!

--- TP2/Tree.java
+++ TP2/Tree.java
@@ -3,20 +3,36 @@ package tree;
 import java.util.List;
 
 public class Tree {
-    private Object value;
+    //private List<Object> listElements = new ArrayList<>();
+    
+    private Object carga;
     private Tree left;
     private Tree right;
 
     public Tree( Object a ) {
-        this.value = a;
+        this.carga = a;
         this.left = null;
         this.right = null;
     }
-
     public List dfs() {
-        return null;
+        List result = new ArrayList();
+        dfsAuxiliar (this, result);
+        return result;
     }
 
+    private void dfsAuxiliar(Tree node, List result) {
+        if (node == null) {
+            return;
+        }
+        result.add(node.carga);
+        if (node.left != null) {
+            dfsAuxiliar(node.left, result);
+        }
+        if (node.right != null) {
+            dfsAuxiliar(node.right, result);
+        }
+    }
+    
     public List bfs() {
         return null;
     }
@@ -32,15 +48,31 @@ public class Tree {
     }
 
     public Tree right() {
+        if (this.right == null) {
+            throw new RuntimeException("Nada en el nodo derecho");
+        }
         return this.right;
     }
 
     public Tree left() {
+        if (this.left == null) {
+            throw new RuntimeException("Nada en el nodo izquierdo");
+        }
         return this.left;
     }
 
     public Object carga() {
-        return this.value;
+        return this.carga;
     }
+
+    // metodos agregados que capaz sirva wu
+    public void addLeft(Tree node) {
+        this.left = node;
+    }
+    public void addRight(Tree node) {
+        this.right = node;
+    }
+    //un public -> tiene hijos? es hoja? 
+
 }
 
