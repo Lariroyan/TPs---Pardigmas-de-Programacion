3eca16747eadaf5bcf55d98abad01c7897d6271f
cami <135481707+cammarze@users.noreply.github.com>
2024-09-20 12:38:27 -0300

    Update Rama.java

--- TP2/Rama.java
+++ TP2/Rama.java
@@ -1,12 +1,17 @@
 package tree;
 
-import java.util.*;
+import java.util.List;
+import java.util.Queue;
 
 public abstract class Rama {
-    public abstract Rama atLeft(Rama rama);
-    public abstract Rama atRight(Rama rama);
+    public Rama() {
+    }
+    public abstract Rama atLeft(Rama var1);
+    public abstract Rama atRight(Rama var1);
     public abstract Object carga();
-    public abstract List<Object> dfs(List<Object> result);
-    public abstract List<Object> bfs(Queue<Rama> queue, List<Object> result);
-}
+    public abstract List dfs(List var1);
+    public abstract List bfs(Queue<Rama> var1, List var2);
+    public abstract Rama left ();
+    public abstract Rama right ();
 
+}
