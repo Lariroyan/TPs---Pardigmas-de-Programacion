3007238c1a695a97fed8e2f0eed6c2933fcfda52
cami <135481707+cammarze@users.noreply.github.com>
2024-09-20 12:38:53 -0300

    Update Nodo.java

--- TP2/Nodo.java
+++ TP2/Nodo.java
@@ -1,5 +1,8 @@
 package tree;
-import java.util.*;
+
+import java.util.List;
+import java.util.Queue;
+
 public class Nodo extends Rama {
     private Object carga;
     private Rama left;
@@ -10,37 +13,45 @@ public class Nodo extends Rama {
         this.left = new RamaVacia();
         this.right = new RamaVacia();
     }
-
     @Override
     public Rama atLeft(Rama rama) {
         this.left = rama;
         return this;
     }
-
     @Override
     public Rama atRight(Rama rama) {
         this.right = rama;
         return this;
     }
-
     @Override
     public Object carga() {
         return this.carga;
     }
-
     @Override
-    public List<Object> dfs(List<Object> result) {
-        result.add(this.carga); // Agrega la carga actual al resultado
-        left.dfs(result); // Explora la rama izquierda
-        right.dfs(result); // Explora la rama derecha
+    public List dfs(List result) {
+        result.add(this.carga);
+        this.left.dfs(result);
+        this.right.dfs(result);
         return result;
     }
-
     @Override
-    public List<Object> bfs(Queue<Rama> queue, List<Object> result) {
+    public List bfs(Queue<Rama> queue, List result) {
         result.add(this.carga);
         queue.add(this.left);
         queue.add(this.right);
         return result;
     }
+
+    @Override
+    public Rama left() {
+        return this.left;
+    }
+
+    @Override
+    public Rama right() {
+        return this.right;
+    }
+
+
 }
+
