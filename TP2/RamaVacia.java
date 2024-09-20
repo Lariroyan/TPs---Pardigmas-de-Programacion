d73d484552c5eb04c71814f970d26bf6073f344d
cami <135481707+cammarze@users.noreply.github.com>
2024-09-20 12:38:00 -0300

    Update RamaVacia.java

--- TP2/RamaVacia.java
+++ TP2/RamaVacia.java
@@ -1,30 +1,41 @@
 package tree;
 
-import java.util.*;
-public class RamaVacia extends Rama{
+import java.util.List;
+import java.util.Queue;
+
+public class RamaVacia extends Rama {
+
+    public RamaVacia() {
+    }
+
+
     @Override
     public Rama atLeft(Rama rama) {
         throw new RuntimeException("Nada a la siniestra!");
     }
-
     @Override
     public Rama atRight(Rama rama) {
         throw new RuntimeException("Nada a la diestra!");
     }
-
-
+    @Override
+    public Rama right() {
+        throw new RuntimeException("Nada a la diestra!");
+    }
+    @Override
+    public Rama left() {
+        throw new RuntimeException("Nada a la siniestra!");
+    }
     @Override
     public Object carga() {
         throw new RuntimeException("Rama vacía no tiene carga!");
     }
 
     @Override
-    public List<Object> dfs(List<Object> result) {
-        return result; // Nada que agregar para una rama vacía
+    public List dfs(List result) {
+        return result;
     }
-
     @Override
-    public List<Object> bfs(Queue<Rama> queue, List<Object> result) {
+    public List bfs(Queue<Rama> queue, List result) {
         return result;
     }
 
