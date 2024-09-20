1c8b3bb85d3d06ae57bc6a0e39ea1ff3e0c57c46
Lari <94306835+Lariroyan@users.noreply.github.com>
2024-09-20 16:24:14 -0300

    Update Tree.java

--- TP2/Tree.java
+++ TP2/Tree.java
@@ -1,65 +1,62 @@
 package tree;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
-
+import java.util.*;
 public class Tree {
     private Rama root;
 
     public Tree(Object carga) {
-        this.root = new Nodo(carga);
+        this.root = new RamaNoVacia(carga);
     }
 
-    public List dfs() {
-        List result = new ArrayList();
-        this.root.dfs(result);
+    public List<Object> dfs() {
+        List<Object> result = new ArrayList<>();
+        dfsAuxiliar(this.root, result);
         return result;
     }
 
-    public List bfs() {
-        List result = new ArrayList();
-        Queue<Rama> queue = new LinkedList();
+    private void dfsAuxiliar(Rama node , List<Object> result) {
+        if (node.esNula()) return;  // No hacemos nada si es una arista nula
+
+        result.add(node.carga());
+        dfsAuxiliar(node.left(), result);
+        dfsAuxiliar(node.right(), result);
+    }
+    public List<Object> bfs() {
+        List<Object> result = new ArrayList<>();
+        Queue<Rama> queue = new LinkedList<>();
         queue.add(this.root);
 
         while (!queue.isEmpty()) {
-            Rama node = queue.poll();
-            node.bfs(queue, result);
+            Rama node= queue.poll();
+            if (!node.esNula()) {
+                result.add(node.carga());
+                queue.add(node.left());
+                queue.add(node.right());
+            }
         }
+
         return result;
     }
 
-public Tree atLeft(Tree b) {
-        this.root = this.root.atLeft(b.root);
+    public Tree atLeft(Tree b) {
+        ((RamaNoVacia) this.root).atLeft(b.root);
         return this;
     }
 
     public Tree atRight(Tree b) {
-        this.root = this.root.atRight(b.root);
+        ((RamaNoVacia) this.root).atRight(b.root);
         return this;
     }
 
+    public Rama left() {
+        return root.left();
+    }
+
+    public Rama right() {
+        return root.right();
+    }
     public Object carga() {
-        return this.root.carga();
-    }
-
-    public Tree left() {
-//        Rama ramaIzquierda = this.root.left();
-//        if (ramaIzquierda instanceof RamaVacia) {
-//            throw new RuntimeException("Nada a la siniestra!");
-//        }
-//        return new Tree(ramaIzquierda.carga());
-        return new Tree(this.root.left());
-    }
-
-    public Tree right() {
-//        Rama ramaDerecha = this.root.right();
-//        if (ramaDerecha instanceof RamaVacia) {
-//            throw new RuntimeException("Nada a la diestra!");
-//        }
-//        return new Tree(ramaDerecha.carga());
-        return new Tree(this.root.right());
+        return root.carga();
     }
 
 }
