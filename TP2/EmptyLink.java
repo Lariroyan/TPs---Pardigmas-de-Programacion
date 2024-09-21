45666f76041384ec7d0aac55902f94367366144a
Lari <94306835+Lariroyan@users.noreply.github.com>
2024-09-21 17:38:14 -0300

    Update EmptyLink.java

--- TP2/EmptyLink.java
+++ TP2/EmptyLink.java
@@ -1,19 +1,16 @@
 package tree;
 import java.util.*;
+
 public class EmptyLink extends Link {
     @Override
     Tree getTree(String side) {
         throw new RuntimeException("Nada a la " + side + "!");
     }
-
     @Override
     List<Object> dfs() {
         return new ArrayList<>();
     }
-
-
-
     public void addToQueue(Queue<Tree> queue) {
-        // No se añade nada a la cola en caso de rama vacía
+        
     }
 }
