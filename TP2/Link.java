0339b9736b2b83c5d95b2580ce95bbbea41a65e8
Lari <94306835+Lariroyan@users.noreply.github.com>
2024-09-21 17:37:51 -0300

    Update Link.java

--- TP2/Link.java
+++ TP2/Link.java
@@ -1,10 +1,9 @@
 package tree;
 import java.util.*;
+
 public abstract class Link {
-    abstract Tree getTree(String side); // Método para obtener el árbol o lanzar la excepción.
-
-    abstract List<Object> dfs();
-
-    abstract void addToQueue(Queue<Tree> queue);
+    public abstract Tree getTree(String side); 
+    public abstract List<Object> dfs();
+    public abstract void addToQueue(Queue<Tree> queue);
 }
 
