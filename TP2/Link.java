798e8b011777670677043bbe062564d920a0f5bc
cami <135481707+cammarze@users.noreply.github.com>
2024-09-22 20:19:53 -0300

    Update Link.java

--- TP2/Link.java
+++ TP2/Link.java
@@ -2,7 +2,7 @@ package tree;
 import java.util.*;
 
 public abstract class Link {
-    public abstract Tree getTree(String side); 
+    public abstract Tree getSubTree(String side); 
     public abstract List<Object> dfs();
     public abstract void addToQueue(Queue<Tree> queue);
 }
