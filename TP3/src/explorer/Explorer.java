f40386c9fcac937295d6a76a46f25e82fdbd782d
cami <135481707+cammarze@users.noreply.github.com>
2024-11-03 14:01:00 -0300

    final ver

--- TP3/src/explorer/Explorer.java
+++ TP3/src/explorer/Explorer.java
@@ -5,8 +5,6 @@ public class Explorer {
     private Direction direction;
     private List<Command> commands = new ArrayList();
     public HatchState hatchState;
-//    private boolean upperHatchOpen = false;
-//    private boolean lowerHatchOpen = false;
 
     public Explorer(int initialX, int initialY, Direction initialDirection) {
         coordinate = new Position (initialX, initialY);
@@ -47,59 +45,28 @@ public class Explorer {
     }
 
     public void openUpperHatch() {
-//        if (lowerHatchOpen) {
-//            throw new IllegalStateException("Cannot open upper hatch when lower hatch is open");
-//        }
-//        upperHatchOpen = true;
         hatchState.openUpper(this);
     }
 
     public void openLowerHatch() {
-//        if (upperHatchOpen) {
-//            throw new IllegalStateException("Cannot open lower hatch when upper hatch is open");
-//        }
-//        lowerHatchOpen = true;
         hatchState.openLower(this);
     }
 
     public void closeHatches() {
-//        if (upperHatchOpen) {
-//            upperHatchOpen = false;
-//        } else if (lowerHatchOpen) {
-//            lowerHatchOpen = false;
-//        } else {
-//            throw new IllegalStateException("No hatch is open to close");
-//        }
         hatchState.close(this);
     }
 
     public void aspirate() {
-//        if (!upperHatchOpen) {
-//            throw new IllegalStateException("Cannot aspirate with upper hatch closed");
-//        }
         hatchState.aspirate(this);
     }
 
     public void collectSample() {
-//        if (!lowerHatchOpen) {
-//            throw new IllegalStateException("Cannot dig with lower hatch closed");
-//        }
         hatchState.collectSample(this);
     }
 
     public void setHatchState(HatchState state) {
         this.hatchState = state;
     }
-//    public HatchState getHatchState() {
-//        return hatchState;
-//    }
-//    public boolean isUpperHatchOpen() {
-//        return hatchState.isUpperHatchOpen(); //-> en test rover.hacthcState.isUpperHatchOpen()
-//    }
-//
-//    public boolean isLowerHatchOpen() {
-//        return hatchState.isLowerHatchOpen();
-//    }
 
     public int getCoordinateX () {return coordinate.coordenadaX();}
     public int getCoordinateY () {return coordinate.coordenadaY();}
